fdbf870badccec0539db784b7ce4799f3f8d08e7
hitomi <hitomi@localhost>
2026-03-11 22:56:21 +0900

    全文検索機能の途中

--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lesson.memo.model.Memo;
@@ -29,11 +30,19 @@ public class MemoController {
     private MemoRepository memoRepository;
 
     @GetMapping
-    public String list(Model model) {
-        List<Memo> memos = memoRepository.findAll();
-        model.addAttribute("memos", memos);
-        return "memo-list";
-    }
+    public String list(
+    		@RequestParam(required = false) String keyword,
+    		Model model) {
+    		List<Memo> memos;
+    		    if (keyword == null || keyword.isEmpty()) {
+    		        memos = memoRepository.findAll();
+    		       } else {
+    		       memos = memoRepository.findByTitleContaining(keyword);
+    		       }
+    		model.addAttribute("memos", memos);
+    		return "memo-list";
+    		}
+
 
     @GetMapping("/new")
     public String showForm(Model model) {
