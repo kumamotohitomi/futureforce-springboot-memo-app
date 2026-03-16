031fe355e61aff2e7f7b04ee1d8fa0351a42f3d3
hitomi <hitomi@localhost>
2026-03-16 15:41:48 +0900

    Adminリポジトリ変更

--- src/main/java/com/lesson/memo/repository/AdimnRepository.java
+++ src/main/java/com/lesson/memo/repository/AdimnRepository.java
@@ -6,6 +6,6 @@ import com.lesson.memo.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-　　Optional<Admin> findByEmail(String Email);
+　　Optional<Admin> findByEmail(String email);
 }
 
