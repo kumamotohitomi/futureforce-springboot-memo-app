291b59abd43fdce9412bc15404fe19bca7ea083a
hitomi <hitomi@localhost>
2026-03-19 10:25:33 +0900

    adminコントローラー

--- src/main/java/com/lesson/memo/controller/AdminController.java
+++ src/main/java/com/lesson/memo/controller/AdminController.java
@@ -1,7 +1,31 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.lesson.memo.model.Admin;
+import com.lesson.memo.repository.AdminRepository;
 
 package com.lesson.memo.controller;
 
+@Controller
 public class AdminController {
+	 @Autowired
+	    private PasswordEncoder passwordEncoder;
+
+	    @Autowired
+	    private AdminRepository adminRepository;
+
+	    @PostMapping("/admin/signup")
+	    public String register(Admin admin) {
+
+	        String encodedPassword = passwordEncoder.encode(admin.getPassword());
+	        admin.setPassword(encodedPassword);
+
+	        adminRepository.save(admin);
+	        return "redirect:/admin/signin";
+	    }
+	}
 
 }
