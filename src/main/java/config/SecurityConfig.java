d5e17c4eed6a357d88af9d06a6d53e89da7f768a
hitomi <hitomi@localhost>
2026-03-19 23:27:03 +0900

    securityconfigのコード追記

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+public class SecurityConfig {
+ @Bean
+  public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder(); //パスワード暗号化
+  }
+
+
+ @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    http.authorizeHttpRequests(authz -> authz
+            .requestMatchers("/admin/signup", "/admin/signin","/css/**", "/js/**").permitAll()
+            .anyRequest().authenticated()
+    ).formLogin(login -> login
+            .loginPage("/admin/signin")//ログインURL
+            .loginProcessingUrl("/login")//ログインボタン押したときの接続先
+            .failureUrl("/admin/signin?error=true")//失敗したら
+            .defaultSuccessUrl("/admin/memos", true)
+            .usernameParameter("email")//メールでログイン
+            .passwordParameter("password")//パスワードログイン
+    ).logout(logout -> logout
+            .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout"))
+            .logoutSuccessUrl("/logout")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+    );
+    return http.build();
+   }
+}
+
