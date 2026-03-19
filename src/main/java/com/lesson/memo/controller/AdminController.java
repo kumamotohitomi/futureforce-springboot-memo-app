feb849b50bd344a0e48c01c42a39785a1c6a3500
hitomi <hitomi@localhost>
2026-03-19 11:36:09 +0900

    admin

--- src/main/java/com/lesson/memo/controller/AdminController.java
+++ src/main/java/com/lesson/memo/controller/AdminController.java
@@ -1,3 +1,4 @@
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/lesson/memo/model/Admin.java
+++ src/main/java/com/lesson/memo/model/Admin.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Admin {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
