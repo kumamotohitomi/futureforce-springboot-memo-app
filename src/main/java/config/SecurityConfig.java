58e9bf1b0a735cb15c799c347df8dab70548e4c8
hitomi <hitomi@localhost>
2026-03-22 22:32:56 +0900

    config調整中

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -7,9 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Service;
 
-@Service
 @Configuration
 public class SecurityConfig {
  @Bean
