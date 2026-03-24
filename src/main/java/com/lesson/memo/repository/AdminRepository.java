6a8cc2a8173d3324c0e19f6bfe58f8be216f7663
hitomi <hitomi@localhost>
2026-03-24 23:37:32 +0900

    spring-security提出１回目

--- src/main/java/com/lesson/memo/repository/AdminRepository.java
+++ src/main/java/com/lesson/memo/repository/AdminRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lesson.memo.model.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+	
     Optional<Admin> findByEmail(String email);
 }
 
